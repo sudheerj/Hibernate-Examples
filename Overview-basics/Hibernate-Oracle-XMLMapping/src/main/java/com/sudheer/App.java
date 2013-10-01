16d6ccc3bb9741471014041610cfaa8795b197fc
sudheerj <sudheer.jonna@gmail.com>
2013-10-01 22:03:12 +0530

    Hibernate Oracle XMLMapping

---
+++ Overview-basics/Hibernate-Oracle-XMLMapping/src/main/java/com/sudheer/App.java
@@ -0,0 +1,24 @@
+package com.sudheer;
+
+import java.util.Date;
+import org.hibernate.Session;
+import com.sudheer.util.HibernateUtil;
+import com.sudheer.user.DBUser;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Maven + Hibernate + Oracle");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		session.beginTransaction();
+		DBUser user = new DBUser();
+
+		user.setUserId(111);
+		user.setUsername("IronMan");
+		user.setCreatedBy("system");
+		user.setCreatedDate(new Date());
+
+		session.save(user);
+		session.getTransaction().commit();
+	}
+}
